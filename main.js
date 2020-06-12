c46b85cb1160a783b91055c6084584f863753f91
Rihards Jukna <rihards.jukna@gmail.com>
2020-06-12 11:34:29 +0100

    Add input validation and optimized committed
    Add statements in click events that validates inputs of zeros, dots and math operators.
    Removed variables assigned to unused dom objects
    Removed variables which linked input with display dom objects which allowed to remove unnacesary code and simplified data validation.
    Replaced 00 with 0 in output div index html to simplify data validation. Now both screens starts with 0.
    This needs to be further adjusted with css to highlight difference between screens for users.
    backspace function is not going to be used twice. Therefore, it is moved into backspace click event to reduce the code.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+// 1. variable section - all dom objects assigned to the global varaibles
 let input = document.querySelector("#input");
 let output = document.querySelector("#output");
-let display = document.querySelector("#display");
 
 const EQUALS = document.querySelector("#equals");
 const ZERO = document.querySelector("#zero");
@@ -9,49 +9,56 @@ const DOT = document.querySelector("#dot");
 const CLEAR = document.querySelector("#clear");
 const BCKSP = document.querySelector("#backspace");
 
-// const ONE = document.querySelector("#one");
-// const TWO = document.querySelector("#two");
-// const THREE = document.querySelector("#three");
-// const FOUR = document.querySelector("#four");
-// const FIVE = document.querySelector("#five");
-// const SIX = document.querySelector("#six");
-// const SEVEN = document.querySelector("#seven");
-// const EIGTH = document.querySelector("#eight");
-// const NINE = document.querySelector("#nine");
-
-// const PLUSS = document.querySelector("#add");
-// const MINUS = document.querySelector("#substract");
-// const MULTI = document.querySelector("#multiply");
-// const DIV = document.querySelector("#divide");
-
 let num = document.querySelectorAll(".number");
 let oper = document.querySelectorAll(".operator");
 
+// 2. input section - contains functions that are responsible for input operations
 
-let inputString = "";
-let formula = "";
+// 2.1 expression and doubleOperator ensures that input is correct and ready for calculation
+function expression(element) {
+    if (input.innerHTML === "0" && /\d/.test(element.value)) { input.innerHTML = input.innerHTML.replace("0", "") };
+    if (output.innerHTML === "0" && /\d/.test(element.value)) { output.innerHTML = output.innerHTML.replace("0", "") };
 
-let expression = element => {
-    formula += element.value;
-    output.innerHTML = formula;
-
-    test(element)
-    input.innerHTML = inputString;  
+    (/[\d\.]/.test(element.value) && /[\d\.]/.test(input.innerHTML) ) ? input.innerHTML += element.value : input.innerHTML = element.value; 
+    output.innerHTML += element.value;
 }
 
-// resets the value of the function and both displays
-let reset = () => {
-    formula = inputString = "";
+function doubleOperators(oper) {
+   ( oper.value === "-" && /[\+\/\*]$/.test(output.innerHTML)) ? output.innerHTML += oper.value : output.innerHTML = output.innerHTML.replace(/\D+$/, oper.value)
+    input.innerHTML = oper.value;
+}
+
+// 2.2 resets the value of the function and both displays
+function reset() {
     input.innerHTML = "0";
-    output.innerHTML = "00";
+    output.innerHTML = "0";
 }
 
-
-
-// click events
-num.forEach( nr =>  nr.addEventListener("click", ()=>  expression(nr)) );
-oper.forEach( op =>  op.addEventListener("click", ()=> expression(op)) );
+// 2.3 click events
+num.forEach( nr =>  nr.addEventListener("click", () => expression(nr)) );
+oper.forEach( op =>  op.addEventListener("click", () => {
+    /[\+\-\*\/]$/.test(output.innerHTML) ? doubleOperators(op) : expression(op);
+}));
 
 CLEAR.addEventListener("click", () => reset());
- 
- 
+BCKSP.addEventListener("click", () => {
+    input.innerHTML.length === 1 ? input.innerHTML = "0" : input.innerHTML = input.innerHTML.slice(0, -1);
+    output.innerHTML.length === 1 ? output.innerHTML = "0" : output.innerHTML = output.innerHTML.slice(0, -1);
+});
+
+ZERO.addEventListener("click", () => { 
+    arr = output.innerHTML.split(/[\+\-\*\/]/);
+   if (arr[arr.length-1].indexOf("0") !== 0 || arr[arr.length-1].indexOf(".") > -1) {expression(ZERO)};
+});
+
+DOT.addEventListener("click", () => {
+    arr = output.innerHTML.split(/[\+\-\*\/]/);
+    /[\+\-\*\/]$/.test(output.innerHTML) ? DOT.value = "0." : DOT.value = "."; 
+    if (arr[arr.length-1].indexOf(".") === -1) {expression(DOT)};  
+}); 
+
+
+
+// EQUALS.addEventListener("click", equation);
+
+
